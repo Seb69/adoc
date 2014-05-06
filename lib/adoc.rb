2eef12d7a041fce56de3a88bd52a20be214ddb96
Sergio Arbeo <serabe@gmail.com>
2014-05-06 19:56:22 +0200

    Add included and extended hooks.

--- lib/adoc.rb
+++ lib/adoc.rb
@@ -1,5 +1,20 @@
 require "adoc/version"
 
 module Adoc
-  # Your code goes here...
+
+  def self.included klazz
+    klazz.extend ClassMethods
+    klazz.include InstanceMethods
+  end
+
+  def self.extended klazz
+    klazz.extend ClassMethods
+    klazz.include InstanceMethods
+  end
+
+  module InstanceMethods
+  end
+
+  module ClassMethods
+  end
 end
--- test/test_adoc.rb
+++ test/test_adoc.rb
@@ -1,11 +1,32 @@
 require 'minitest_helper'
 
 class TestAdoc < MiniTest::Unit::TestCase
-  def test_that_it_has_a_version_number
-    refute_nil ::Adoc::VERSION
+  def test_that_it_includes_instance_methods_when_included
+    a = Class.new
+    a.include Adoc
+    assert_includes a.ancestors, Adoc::InstanceMethods
   end
 
-  def test_it_does_something_useful
-    assert false
+  def test_that_it_includes_class_methods_when_included
+    a = Class.new
+    a.include Adoc
+    assert_includes eigenclass(a).ancestors, Adoc::ClassMethods
+  end
+
+
+  def test_that_it_includes_instance_methods_when_extended
+    a = Class.new
+    a.extend Adoc
+    assert_includes a.ancestors, Adoc::InstanceMethods
+  end
+
+  def test_that_it_includes_class_methods_when_extended
+    a = Class.new
+    a.extend Adoc
+    assert_includes eigenclass(a).ancestors, Adoc::ClassMethods
+  end
+
+  def eigenclass klazz
+    class << klazz; self; end
   end
 end
