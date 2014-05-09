28544022ab50126029a1c19d38db665e519b91d2
Sergio Arbeo <serabe@gmail.com>
2014-05-09 02:30:46 +0200

    Fix bug

--- lib/adoc.rb
+++ lib/adoc.rb
@@ -18,7 +18,21 @@ module Adoc
   module ClassMethods
 
     def doc_for method_name
-      doc_cache[method_name.to_sym] || ""
+      method_name = method_name.to_sym
+      return doc_cache[method_name] if has_doc_for? method_name
+      look_for method_name
+    end
+
+    protected def look_for method_name
+      if superclass.respond_to? :doc_for
+        superclass.doc_for method_name
+      else
+        nil
+      end
+    end
+
+    def has_doc_for? method_name
+      doc_cache.has_key? method_name.to_sym
     end
 
     def adoc doc, method_name
--- test/adoc/test_adoc_class_methods.rb
+++ test/adoc/test_adoc_class_methods.rb
@@ -3,19 +3,20 @@ require 'minitest_helper'
 class TestAdocClassMethods < MiniTest::Test
 
   def setup
-    @a_doc = a_doc = "doc"
+    @a_doc = a_doc = "doc" 
     @a = Class.new do
       include Adoc
       adoc a_doc,
-      def my_method
-      end 
+      def my_method; end 
+
+      adoc "something useful",
+      def a_method;end
     end
 
     @b_doc = b_doc = "doc b"
     @b = Class.new(@a) do
       adoc b_doc,
-      def my_method
-      end
+      def my_method;end
     end
 
     refute_equal @a_doc, @b_doc
@@ -28,5 +29,13 @@ class TestAdocClassMethods < MiniTest::Test
 
   def test_that_child_classes_have_its_own_documentation
     assert_equal @b_doc, @b.doc_for(:my_method)
+  end
+
+  def test_that_a_child_class_has_access_to_parent_doc
+    assert_equal @a.doc_for(:a_method), @b.doc_for(:a_method)
+  end
+
+  def test_that_default_value_is_nil
+    assert_nil @b.doc_for(:a_random_method)
   end
 end
